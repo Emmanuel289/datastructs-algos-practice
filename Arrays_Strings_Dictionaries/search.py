117f80f96616419b552f86fbcd782165dcf525c4
Okechukwu Ochia <emmirald@gmail.com>
2020-03-20 18:24:27 -0600

    Deleted a rduendant line

--- Arrays_Strings_Dictionaries/search.py
+++ Arrays_Strings_Dictionaries/search.py
@@ -1,29 +1,11 @@
 
 """
 
-Big Oh analysis
-bin_search has runtime of O(logn) in the best case with sorted arrays and O(n)in the worst case with completely unsorted arrays
 dict_search() has runtime of O(1) with a space complexity of O(n)
 array_search() has runtime of O(n) with a space complexity of O(n)
 
 
 """
-
-def bin_search(input_list, num):  
-	sorted_list =  merge_sort(input_list)  #sort in ascending order
-	
-	mid = len(sorted_list)//2 #midpoint of array
-	while mid>0:  
-		if num < sorted_list[mid]:  #if num is to the left of midpoint
-			sorted_list = sorted_list[:mid] #discard right half 
-		elif num > sorted_list[mid]:  # if num is to the right of midpoint
-			sorted_list = sorted_list[mid:] # discard left part
-		elif num == sorted_list[mid]:
-			print("{} is in {} at index {}".format(num, input_list, mid))
-	
-		else:
-			print("{} is not in {}".format(num, input_list))
-		mid=mid//2
 
 
 def dict_search(input_arr, num):
@@ -94,7 +76,4 @@ print("dict_search gives:")
 dict_search(test, num1)
 dict_search(test, num2)
 
-print("bin_search gives:")
-print(bin_search(test, num1))
-bin_search(test, num2)
 
